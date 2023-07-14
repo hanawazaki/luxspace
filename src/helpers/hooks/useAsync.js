e83d42e4aad458ef6c6ab9aa1669e032e06f9a03
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2023-07-14 20:21:15 +0700

    browse product

--- src/helpers/hooks/useAsync.js
+++ src/helpers/hooks/useAsync.js
@@ -1,25 +1,55 @@
-import { useState, useCallback } from 'react';
+import { useCallback, useReducer, useRef } from 'react';
+import useSafeDispatch from './useSafeDispatch';
 
 const defaultState = {
-    data: null, 
-    status: "idle", 
+    data: null,
+    status: "idle",
     error: null
 }
 export default function useAsync(initialState) {
-    const [{data,status,error}, setState] = useState({ ...defaultState, ...initialState })
+    const initialStateRef = useRef({
+        ...defaultState, ...initialState
+    })
+
+    const [{ data, status, error }, setState] = useReducer((state, action) => {
+        return { ...state, ...action }
+    }, initialStateRef.current)
+
+    const safeSetState = useSafeDispatch(setState)
+
     const run = useCallback(
         (promise) => {
             if (!promise || !promise.then)
                 throw new Error(`The argument passed to useAsyn().run must be a promise`)
-            setState({ status: "pending" })
+            safeSetState({ status: "pending" })
             return promise.then(data => {
-                setState({ data, status: "resoved" })
+                safeSetState({ data, status: "resoved" })
                 return data
             }, error => {
-                setState({ status: "rejected", error: JSON.parse(error.message) })
+                safeSetState({ status: "rejected", error: JSON.parse(error.message) })
             })
         },
-        [setState],
+        [safeSetState],
     );
-    return {data, status, error, run}
+
+    const setData = useCallback(
+        (data) => {
+            safeSetState({ data })
+        },
+        [safeSetState],
+    )
+    const setError = useCallback(
+        (error) => {
+            safeSetState({ error })
+        },
+        [safeSetState],
+    )
+    const reset = useCallback(
+        () => {
+            safeSetState(initialStateRef.current)
+        },
+        [safeSetState],
+    )
+
+    return { data, status, error, run, setData, setError, reset, isIdle: status === "idle", isLoading: status === "idle" || status === "pending", isError: status === "rejected", isSuccess: status === "resolved" }
 }
--- src/parts/HomePage/BrowseRoom.js
+++ src/parts/HomePage/BrowseRoom.js
@@ -1,21 +1,35 @@
 import useAsync from "helpers/hooks/useAsync";
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import fetch from 'helpers/fetch'
 
 export default function BrowseRoom() {
-  const { data, status, error, run } = useAsync({ data: { username: "" } });
+  const { data, status, error, run, isLoading } = useAsync({ data: { username: "" } });
   useEffect(() => {
-   run(
-     fetch("https://f6728700-0d3e-42b2-bd36-59e582ad1708.mock.pstmn.io/api/categories/?page=1&limit=4")
-     .then( async response =>{
-       const jsonResponse = await response.json()
-       if(response.ok) return jsonResponse
-       
-       throw new Error(JSON.stringify(jsonResponse))
-     })
-     )
+    run(
+      fetch({ url: "/api/categories/?page=1&limit=4" })
+    )
   }, [run])
-  console.log(data,status,error)
+  console.log(data, status, error)
+
+  const ratioClassNames = {
+    wrapper: {
+      default: {
+        "1/9": "col-span-9 row-span-1"
+      },
+      md: {
+        "1/4": "md:col-span-4 md:row-span-1",
+        "2/2": "md:col-span-2 md:row-span-2",
+        "2/3": "md:col-span-3 md:row-span-2",
+      },
+    },
+    meta: {
+      "1/9": "left-0 top-0 bottom-0 flex justify-center flex-col pl-48 md:pl-72",
+      "1/4": "left-0 top-0 bottom-0 flex justify-center flex-col pl-48 md:pl-72",
+      "2/2": "inset-0 md:bottom-auto flex justify-center md:items-center flex-col pl-48 md:pl-0 pt-0 md:pt-12",
+      "2/3": "inset-0 md:bottom-auto flex justify-center md:items-center flex-col pl-48 md:pl-0 pt-0 md:pt-12",
+    }
+  }
   return (
     <section className="flex bg-gray-100 py-16 px-4" id="browse-the-room">
       <div className="container mx-auto">
@@ -25,74 +39,32 @@ export default function BrowseRoom() {
             that we designed for you
           </h3>
         </div>
+
         <div className="grid grid-rows-2 grid-cols-9 gap-4">
-          <div
-            className="relative col-span-9 row-span-1 md:col-span-4 card"
-            style={{ height: 180 }}
-          >
-            <div className="card-shadow rounded-xl">
-              <img
-                src="./images/content/image-catalog-1.png"
-                alt=""
-                className="w-full h-full object-cover object-center overlay overflow-hidden rounded-xl"
-              />
-            </div>
-            <div className="overlay left-0 top-0 bottom-0 flex justify-center flex-col pl-48 md:pl-72">
-              <h5 className="text-lg font-semibold">Living Room</h5>
-              <span className="">18.309 items</span>
-            </div>
-            <Link to="/details" className="stretched-link">
-              {/* <!-- fake children --> */}
-            </Link>
-          </div>
-          <div className="relative col-span-9 row-span-1 md:col-span-2 md:row-span-2 card">
-            <div className="card-shadow rounded-xl">
-              <img
-                src="./images/content/image-catalog-3.png"
-                alt=""
-                className="w-full h-full object-cover object-center overlay overflow-hidden rounded-xl"
-              />
-            </div>
-            <div className="overlay right-0 left-0 top-0 bottom-0 md:bottom-auto flex justify-center md:items-center flex-col pl-48 md:pl-0 pt-0 md:pt-12">
-              <h5 className="text-lg font-semibold">Decoration</h5>
-              <span className="">77.392 items</span>
-            </div>
-            <Link to="/details" className="stretched-link">
-              {/* <!-- fake children --> */}
-            </Link>
-          </div>
-          <div className="relative col-span-9 row-span-1 md:col-span-3 md:row-span-2 card">
-            <div className="card-shadow rounded-xl">
-              <img
-                src="./images/content/image-catalog-4.png"
-                alt=""
-                className="w-full h-full object-cover object-center overlay overflow-hidden rounded-xl"
-              />
-            </div>
-            <div className="overlay right-0 left-0 top-0 bottom-0 md:bottom-auto flex justify-center md:items-center flex-col pl-48 md:pl-0 pt-0 md:pt-12">
-              <h5 className="text-lg font-semibold">Living Room</h5>
-              <span className="">22.094 items</span>
-            </div>
-            <Link to="/details" className="stretched-link">
-              {/* <!-- fake children --> */}
-            </Link>
-          </div>
-          <div className="relative col-span-9 row-span-1 md:col-span-4 card">
-            <div className="card-shadow rounded-xl">
-              <img
-                src="./images/content/image-catalog-2.png"
-                alt=""
-                className="w-full h-full object-cover object-center overlay overflow-hidden rounded-xl"
-              />
-            </div>
-            <div className="overlay left-0 top-0 bottom-0 flex justify-center flex-col pl-48 md:pl-72">
-              <h5 className="text-lg font-semibold">Children Room</h5>
-              <span className="">837 items</span>
-            </div>
-            <Link to="/details" className="stretched-link">
-              {/* <!-- fake children --> */}
-            </Link>
-          </div>
+          {
+            isLoading ? "Loading . . . ." : data.data.map((item, index) => {
+              return <div key={item.id}
+
+                className={`relative card ${ratioClassNames?.wrapper.default?.[item.ratio.default]} ${ratioClassNames?.wrapper.md?.[item.ratio.md]}`}
+                style={{ height: index === 0 ? 180 : "auto" }}
+              >
+                <div className="card-shadow rounded-xl">
+                  <img
+                    src={`./images/content/${item.imageUrl}`}
+                    alt={item.title}
+                    className="w-full h-full object-cover object-center overlay overflow-hidden rounded-xl"
+                  />
+                </div>
+                <div className={`overlay ${ratioClassNames?.meta?.[item.ratio.md]}`}>
+                  <h5 className="text-lg font-semibold">{item.title}</h5>
+                  <span className="">{item.products} item{item.products > 1 ? "s" : ""}</span>
+                </div>
+              </div>
+            })
+          }
+
+
+
         </div>
       </div>
     </section>
